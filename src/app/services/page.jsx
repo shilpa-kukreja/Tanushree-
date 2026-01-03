d819e26068c136c82a1846285e0d0b2971558143
Shubham Shukla <shubshukla2332@gmail.com>
2026-01-03 12:58:02 +0530

    some changes in services

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 import { useSearchParams } from "next/navigation";
 import Header from "../componats/Header";
 import Footer from "../componats/Footer";
+import { useRouter } from "next/navigation";
 
 const fade = {
   hidden: { opacity: 0, y: 24 },
@@ -93,8 +94,9 @@ const servicesData = {
 };
 
 export default function ServicesPage() {
-  const searchParams = useSearchParams();
-  const category = searchParams.get("category") || "real-estate";
+   const router = useRouter();
+  
+  const category = router.get("category") || "real-estate";
 
   const current = servicesData[category];
 
