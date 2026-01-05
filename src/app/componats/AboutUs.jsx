dcaf8ad892f5e8b6169b6d60c99460f945ddedad
Shubham Shukla <shubshukla2332@gmail.com>
2026-01-05 14:08:19 +0530

    some changes

--- src/app/componats/AboutUs.jsx
+++ src/app/componats/AboutUs.jsx
@@ -141,7 +141,7 @@ transition-all duration-300 hover:scale-105">
                     <p className="text-gray-500 text-sm">Pushing boundaries since 2010</p>
                   </div>
                 </div>
-                <div className="flex items-center justify-between">
+                {/* <div className="flex items-center justify-between">
                   <div>
                     <div className="text-2xl font-bold text-gray-900">45+</div>
                     <div className="text-gray-500 text-sm">Countries Served</div>
@@ -151,7 +151,7 @@ transition-all duration-300 hover:scale-105">
                       <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd" />
                     </svg>
                   </div>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
